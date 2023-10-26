64dc3d0747c2bbccd5397677c4454f1f03afa17b
j.renzoo <j.renzoo@localhost>
2023-10-26 11:19:59 -0500

    welp

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,28 +12,34 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+
+		// FIXME compute the geometric sum for the first n terms recursive
+		double sum = 0.0;
+		if (n == 0) {
 			return 0;
-		
+		}
+
+		// sum = sum + Math.pow(0.5, power);
+		sum += 1 / Math.pow(2, n) + geometricSum(n - 1);
+
+		return sum;
+
 	}
 
 	/**
-	 * This method uses recursion to compute the greatest common divisor
-	 * for the two input values
+	 * This method uses recursion to compute the greatest common divisor for the two
+	 * input values
 	 * 
 	 * @param p first operand
 	 * @param q second operand
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
-	}
 
-	
+		// FIXME compute the gcd of p and q using recursion
+		return 0;
+
+	}
 
 	/**
 	 * This method uses recursion to create a reverse of the given array
@@ -42,10 +48,11 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+
+		// FIXME create a helper method that can recursively reverse the given array
+
+		return new int[0];
+
 	}
 
 	/**
@@ -58,9 +65,20 @@ public class RecursiveMethods {
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
+			
 			double radiusMinimumDrawingThreshold) {
+		if(radius < radiusMinimumDrawingThreshold) {
+			return;
+		}
+		StdDraw.circle(xCenter, yCenter, radius);
 		
-		// FIXME
+		circlesUponCircles(xCenter + radius, yCenter, radius / 3, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter - radius, yCenter, radius / 3, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter + radius, radius / 3, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, yCenter - radius, radius / 3, radiusMinimumDrawingThreshold);
+		
+
+
 	}
 
 }
